bc6429f2d837b135008d47e47d07b9f0fd579760
RanYiMi <987022228@qq.com>
2020-01-16 21:30:41 +0800

    登陆以及退出功能完毕

---
+++ web_back/js/config.js
@@ -0,0 +1,8 @@
+var BASEURL = 'http://192.168.137.1:8000'
+
+// 用来保存所有的接口信息
+var APILIST = {
+    user_login : BASEURL + '/admin/login',
+    user_logout : BASEURL + '/admin/logout',
+    user_getInfo : BASEURL + '/admin/getuser'
+}
---
+++ web_back/js/packjson.js
@@ -0,0 +1,16 @@
+var user = {
+    login:function(name,password){
+        return $.post(APILIST.user_login,{
+            'user_name' : name,
+            'password' : password
+        })
+    },
+    // 用户退出
+    logout:function(){
+        return $.post(APILIST.user_logout)
+    },
+    // 获取信息
+    getInfo:function(){
+        return $.get(APILIST.user_getInfo)
+    }
+}
